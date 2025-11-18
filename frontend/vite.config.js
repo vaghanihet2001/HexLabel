a67749f78896c11290c17736f321066caf5085f3
hetv-weboccult <het.vaghani@weboccult.com>
2025-11-18 12:17:23 +0530

    config changes to allow different hostname for deploy web app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({
-  plugins: [react()],
-  server: { host: '0.0.0.0', port: 5173 }
-})
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const allowedHosts = env.VITE_ALLOWED_HOSTS
+    ? env.VITE_ALLOWED_HOSTS.split(",")
+    : [];
+
+  return {
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0",
+      port: process.env.PORT || 5000,
+      allowedHosts,
+    },
+  };
+});
